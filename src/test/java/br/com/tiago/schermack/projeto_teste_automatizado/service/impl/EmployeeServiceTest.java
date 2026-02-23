723a9dd35c4afcb94110073f07e32d3b12b6d48e
Gustavo Henrique Fiamoncini <gustavohf092006@gmail.com>
2026-02-23 19:58:40 -0300

    Feat: Testes unitários do service finalizados

--- src/test/java/br/com/tiago/schermack/projeto_teste_automatizado/service/impl/EmployeeServiceTest.java
+++ src/test/java/br/com/tiago/schermack/projeto_teste_automatizado/service/impl/EmployeeServiceTest.java
@@ -1,25 +1,26 @@
 package br.com.tiago.schermack.projeto_teste_automatizado.service.impl;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.mockito.ArgumentMatchers.any;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
-import java.util.Optional;
-
 import org.springframework.boot.test.context.SpringBootTest;
 
 import br.com.tiago.schermack.projeto_teste_automatizado.dto.EmployeeRequestDTO;
 import br.com.tiago.schermack.projeto_teste_automatizado.dto.EmployeeResponseDTO;
 import br.com.tiago.schermack.projeto_teste_automatizado.entity.Employee;
 import br.com.tiago.schermack.projeto_teste_automatizado.repository.EmployeeRepository;
+import jakarta.persistence.EntityNotFoundException;
 
 @SpringBootTest
 class EmployeeServiceTest {
@@ -78,5 +79,48 @@ class EmployeeServiceTest {
         verify(employeeRepository, times(1)).save(any(Employee.class));
 
 
+    }
+
+    @Test
+    @DisplayName("Deve retornar erro ao atualizar usuário não existente")
+    public void deveDarErroQuandoAtualizarFuncionarioQueNaoExistir(){
+
+        //Arrange
+        Long id = 1L;
+        EmployeeRequestDTO requestDTO = new EmployeeRequestDTO("Gugu", "gugu@email.com");
+
+        when(employeeRepository.findById(id)).thenReturn(Optional.empty());
+
+        //Act + Assert
+        assertThrows(EntityNotFoundException.class, () -> {
+            employeeService.update(id, requestDTO);
+        });
+
+        // Verifica se save não foi chamado
+        verify(employeeRepository, never()).save(any(Employee.class));
+
+
+    }
+
+    @Test
+    public void deveEcontrarUmFuncionarioComSucesso(){
+        //Arrange
+        Long id = 1L;
+        Employee employee = new Employee("Gustavo", "gustavo@gmail.com");
+        employee.setId(id);
+
+        when(employeeRepository.findById(id))
+            .thenReturn(Optional.of(employee));
+
+        //Act
+        EmployeeResponseDTO responseDTO = employeeService.findById(id);
+
+        //Assert
+        assertNotNull(responseDTO);
+        assertEquals(id, responseDTO.id());
+        assertEquals("Gustavo", responseDTO.firstName());
+        assertEquals("gustavo@gmail.com", responseDTO.email());
+
+        verify(employeeRepository, times(1)).findById(id);
     }
 }
